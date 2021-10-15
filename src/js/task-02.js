d6f1b7ab6d5b08ec2c0972ba70a7c1be4dc4f180
Albert Takács <albitakacs@gmail.com>
2021-10-15 18:35:47 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -7,31 +7,25 @@ const ingredients = [
   'Condiments',
 ];
 
+
+
 const ingredientsEl = document.querySelector('#ingredients');
 
-const firstItemEl = document.createElement("li");  
-firstItemEl.textContent = ingredients[0];
-firstItemEl.classList.add("item");
+const createElements = element => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = element;
+  return itemEl;
+};
+const elementListRef = ingredients.map(ingredient =>
+  createElements(ingredient),
+);
 
-const secondItemEl = document.createElement("li");  
-secondItemEl.textContent = ingredients[1];
-secondItemEl.classList.add("item");
+ingredientsEl.append(...elementListRef);
 
-const thirdItemEl = document.createElement("li");  
-thirdItemEl.textContent = ingredients[2];
-thirdItemEl.classList.add("item");
+  
 
-const foursItemEl = document.createElement("li");  
-foursItemEl.textContent = ingredients[3];
-foursItemEl.classList.add("item");
 
-const fifthItemEl = document.createElement("li");  
-fifthItemEl.textContent = ingredients[4];
-fifthItemEl.classList.add("item");
 
-const sixthItemEl = document.createElement("li");  
-sixthItemEl.textContent = ingredients[5];
-sixthItemEl.classList.add("item");
 
-ingredientsEl.append(firstItemEl, secondItemEl, thirdItemEl,
-  foursItemEl, fifthItemEl, sixthItemEl);
+
+
