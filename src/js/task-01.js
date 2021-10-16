bb63d4a119cc379f6566042166aae15ab3c9f296
Albert Takács <albitakacs@gmail.com>
2021-10-16 18:05:46 +0200

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,28 +1,14 @@
 const categoriesItemsEl = document.querySelectorAll('#categories .item');
 console.log("Number of categories:",categoriesItemsEl.length);
 
-const firstTitleEl = categoriesItemsEl[0].firstElementChild;
-console.log('Category:', firstTitleEl.textContent);
-
-const firstListEl = firstTitleEl.nextElementSibling;
-
-const firstListItemsEl = firstListEl.children;
-console.log("Elements:", firstListItemsEl.length)
-
-const secondTitleEl = categoriesItemsEl[1].firstElementChild;
-console.log('Category:', secondTitleEl.textContent);
-
-const secondListEl = secondTitleEl.nextElementSibling;
-
-const secondListItemsEl = secondListEl.children;
-console.log("Elements:", secondListItemsEl.length);
-
-const thirdTitleEl = categoriesItemsEl[2].firstElementChild;
-console.log('Category:', thirdTitleEl.textContent);
-
-const thirdListEl = thirdTitleEl.nextElementSibling;
-
-const thirdListItemsEl = thirdListEl.children;
-console.log("Elements:", thirdListItemsEl.length)
+for (const element of categoriesItemsEl) {
+    
+    const titleEl = element.firstElementChild.textContent;
+    console.log(`Category: ${titleEl}`)
+    
+    const listEl = element.lastElementChild.children.length;
+    console.log(`Elements: ${listEl}`);
+    
+}
 
 
